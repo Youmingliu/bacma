be16cf6732cdcb06db3c95f09535ddbec5164a65
Ryan Chen <ryanchen@yahoo-inc.com>
2015-05-29 00:51:00 -0700

    First initial template for BACMA website

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+var bacmaName = 'Bay Area Chinese Musician Association';
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: bacmaName, name: bacmaName });
+});
+
+module.exports = router;
